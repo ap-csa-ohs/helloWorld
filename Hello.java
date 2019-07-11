92e6b008891baf9b00b9f1ba5e02858240a5f44a
Chris Waage <40725414+cwaage@users.noreply.github.com>
2019-07-10 19:46:49 -0700

    Hello World Starter Code

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*
+
+* Hello World
+* Your First Java Program
+* Description: Program prints a simple message to the console
+* Author:
+* Date/Version: 
+
+*/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+	// Your Code Goes Here
+
+
+	}
+
+
+}
